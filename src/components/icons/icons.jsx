929765990dd16b046e2bebb7bb280a96fbfbb89d
Bruno Krugel <brunokrugel@gmail.com>
2023-04-24 19:18:38 -0300

    Rename

--- src/components/icons/icons.jsx
+++ src/components/icons/icons.jsx
@@ -1,13 +1,13 @@
 function Check() {
-  return <img src="/plr-page/icons/check.svg" alt="check" width={140} />;
+  return <img src="/rmktpzb/icons/check.svg" alt="check" width={140} />;
 }
 
 function Play() {
-  return <img src="/plr-page/icons/play.svg" alt="play" width={140} />;
+  return <img src="/rmktpzb/icons/play.svg" alt="play" width={140} />;
 }
 
 function Credit() {
-  return <img src="/plr-page/icons/credit.svg" alt="credit" width={140} />;
+  return <img src="/rmktpzb/icons/credit.svg" alt="credit" width={140} />;
 }
 
 export { Check, Play, Credit };
