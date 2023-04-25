85705e2e86e38a20bc4c8750a0c91fad4f9bebc5
Bruno Krugel <brunokrugel@gmail.com>
2023-04-25 14:20:28 -0300

    Purge css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,25 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://BrunoKrugel.github.io",
   base: "/rmktpzb",
   scripts: {
-    "text/javascript": ["**/*.js"],
+    "text/javascript": ["**/*.js"]
   },
-  integrations: [react()],
+  integrations: [react(), purgecss()],
   vite: {
     build: {
       rollupOptions: {
         output: {
           entryFileNames: "entry.[hash].js",
           chunkFileNames: "chunks/chunk.[hash].js",
-          assetFileNames: "assets/asset.[hash][extname]",
-        },
-      },
-    },
-  },
+          assetFileNames: "assets/asset.[hash][extname]"
+        }
+      }
+    }
+  }
 });
