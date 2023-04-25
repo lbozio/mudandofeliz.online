628971dd130d2d12c59b1a240b4111e43d184fb8
Bruno Krugel <brunokrugel@gmail.com>
2023-04-25 14:35:21 -0300

    img optimizer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,26 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import purgecss from "astro-purgecss";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://BrunoKrugel.github.io",
   base: "/rmktpzb",
   scripts: {
-    "text/javascript": ["**/*.js"]
+    "text/javascript": ["**/*.js"],
   },
-  integrations: [react(), purgecss()],
+  integrations: [react(), compress(), purgecss()],
   vite: {
     build: {
       rollupOptions: {
         output: {
           entryFileNames: "entry.[hash].js",
           chunkFileNames: "chunks/chunk.[hash].js",
-          assetFileNames: "assets/asset.[hash][extname]"
-        }
-      }
-    }
-  }
+          assetFileNames: "assets/asset.[hash][extname]",
+        },
+      },
+    },
+  },
 });
