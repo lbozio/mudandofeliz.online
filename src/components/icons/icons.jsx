4019c474080511e797bf1e19ca5016d95017178d
inforise <65975581+lbozio@users.noreply.github.com>
2023-04-25 00:38:43 +0000

    remove base

--- src/components/icons/icons.jsx
+++ src/components/icons/icons.jsx
@@ -1,13 +1,13 @@
 function Check() {
-  return <img src="/rmktpzb/icons/check.svg" alt="check" width={140} />;
+  return <img src="/icons/check.svg" alt="check" width={140} />;
 }
 
 function Play() {
-  return <img src="/rmktpzb/icons/play.svg" alt="play" width={140} />;
+  return <img src="/icons/play.svg" alt="play" width={140} />;
 }
 
 function Credit() {
-  return <img src="/rmktpzb/icons/credit.svg" alt="credit" width={140} />;
+  return <img src="/icons/credit.svg" alt="credit" width={140} />;
 }
 
 export { Check, Play, Credit };
